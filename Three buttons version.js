a0b8a0b7b5646ba0cd6b2ff9fddcdae9392c4f10
DorianWelcode <104610609+DorianWelcode@users.noreply.github.com>
2022-05-30 17:52:10 +0300

    Update Three buttons version.js

--- Three buttons version.js
+++ Three buttons version.js
@@ -1,5 +1,5 @@
-function verifica(buton) {
-			if (buton == Math.floor(Math.random() * 3) + 1) {
+function verifica(nr) {
+			if (nr == Math.floor(Math.random() * 3) + 1) {
 				return alert('BUTONUL ALES ESTE CASTIGATOR');
 			}
 			return alert('BUTONUL ALES NU E CASTIGATOR');
