7dabc7611d7bf4a976709793d7d3d579e0e1942d
DorianWelcode <104610609+DorianWelcode@users.noreply.github.com>
2022-05-30 17:49:22 +0300

    Create Three buttons version.js

---
+++ Three buttons version.js
@@ -0,0 +1,6 @@
+function verifica(buton) {
+			if (buton == Math.floor(Math.random() * 3) + 1) {
+				return alert('BUTONUL ALES ESTE CASTIGATOR');
+			}
+			return alert('BUTONUL ALES NU E CASTIGATOR');
+		}
